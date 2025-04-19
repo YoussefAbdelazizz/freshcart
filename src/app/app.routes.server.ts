1a941ae9fc2f6e63322e32c0dcb9f4ad99cc9283
YoussefAbdelazizz <pergostudy@gmail.com>
2025-04-19 11:00:30 +0200

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,13 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'product-details/:p_id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'checkout/:c_id',
+    renderMode: RenderMode.Client
   }
 ];
